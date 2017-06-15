7ae2e03d69fa9d69975108963dcc896321367baa
Bill42362 <bill42362@gmail.com>
2017-06-15 12:32:32 +0800

    Enable `:hover` effect on HeaderBarNavItem.

--- src/HeaderBarNavItem.js
+++ src/HeaderBarNavItem.js
@@ -5,23 +5,35 @@ import React from 'react';
 class HeaderBarNavItem extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {isHovered: false};
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave = this.onMouseLeave.bind(this);
     }
+    onMouseEnter(e) { this.setState({isHovered: true}); }
+    onMouseLeave(e) { this.setState({isHovered: false}); }
     render() {
         const { child } = this.props;
+        const { isHovered } = this.state;
         const itemStyle = {
             borderBottom: `3px solid ${child.props['data-color']}`,
         };
         const matchedLocation = location.href.match(child.props['data-match']);
         if(matchedLocation) { itemStyle.backgroundColor = child.props['data-color']; }
+        if(isHovered && !matchedLocation) {
+            itemStyle.color = child.props['data-color'];
+            itemStyle.fontWeight = '500';
+        }
         let content = child;
         switch(child.type) {
             case 'a':
             default:
                 break;
         }
-        return <div className='header-bar-nav-item' style={itemStyle} >
-            {content}
-        </div>;
+        return <div
+            className='header-bar-nav-item' style={itemStyle}
+            onMouseEnter={this.onMouseEnter}
+            onMouseLeave={this.onMouseLeave}
+        >{content}</div>;
     }
 }
 
