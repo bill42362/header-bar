0bac614c26d1dacfdecfc4d1902938dc3b8accff
Bill42362 <bill42362@gmail.com>
2017-06-16 17:40:42 +0800

    Done menu appearing animation.

--- src/js/index.js
+++ src/js/index.js
@@ -10,9 +10,15 @@ const defaultProps = {
 class HeaderBar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {isMenuOpen: false};
+        this.openMenu = this.openMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
     }
+    openMenu() { this.setState({isMenuOpen: true}); }
+    closeMenu() { this.setState({isMenuOpen: false}); }
     render() {
         const { style, logo, hamburger, menuCloser } = this.props;
+        const { isMenuOpen } = this.state;
         let { children } = this.props;
         if(!children.length) { children = [children]; }
         const navs = children.filter(child => { return !child.props['data-subnav']; });
@@ -30,15 +36,29 @@ class HeaderBar extends React.Component {
             </nav>
             <div className='header-bar-collapse'>
                 <div className='header-bar-collapse-placeholder'></div>
-                {!!hamburger && <img className='header-bar-collapse-hamburger' {...hamburger}></img>}
-                {!hamburger && <div className='header-bar-collapse-hamburger'>≡</div>}
+                {!!hamburger && <img
+                    {...hamburger}
+                    className='header-bar-collapse-hamburger'
+                    role='button' onClick={this.openMenu}
+                />}
+                {!hamburger && <div
+                    className='header-bar-collapse-hamburger'
+                    role='button' onClick={this.openMenu}
+                >≡</div>}
             </div>
-            <div className='header-bar-collapse-menu-wrapper'>
-                <div className='header-bar-collapse-menu-frame'></div>
+            {isMenuOpen && <div className='header-bar-collapse-menu-wrapper'>
+                <div className='header-bar-collapse-menu-frame' role='button' onClick={this.closeMenu}></div>
                 <div className='header-bar-collapse-menu'>
                     <div className='header-bar-collapse-menu-header'>
-                        {!!menuCloser && <img className='header-bar-collapse-menu-closer' {...menuCloser}></img>}
-                        {!menuCloser && <div className='header-bar-collapse-menu-closer'>✖︎</div>}
+                        {!!menuCloser && <img
+                            {...menuCloser}
+                            className='header-bar-collapse-menu-closer'
+                            role='button' onClick={this.closeMenu}
+                        />}
+                        {!menuCloser && <div
+                            className='header-bar-collapse-menu-closer'
+                            role='button' onClick={this.closeMenu}
+                        >✖︎</div>}
                     </div>
                     <nav className='header-bar-collapse-menu-nav'>
                         {navs.map((nav, index) => {
@@ -54,7 +74,7 @@ class HeaderBar extends React.Component {
                         })}
                     </nav>
                 </div>
-            </div>
+            </div>}
         </div>;
     }
 }
